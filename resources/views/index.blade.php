57af17535d09fff5c3ae3cc2f4b900aa52d7ec30
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-11-24 02:25:54 +0330

    update sidebar with @parent show all old data in sidebar and new data attach the end of the index  sidebar

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -53,3 +53,16 @@
     </li>
   </ul>
 @endsection
+
+
+@section('sidebar')
+@parent
+
+<div class="card my-4">
+  <h5 class="card-header bg-primary">Index Side Widget</h5>
+  <div class="card-body">
+    You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+  </div>
+</div>
+    
+@endsection
