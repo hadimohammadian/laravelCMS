8f1cdbeabd1abc5e6feb141dc07c5d1d56ce43a2
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-11-24 02:29:29 +0330

    add sidebar to service page that show only write in this page and don't show the old sidebar in master page

---
+++ resources/views/service.blade.php
@@ -0,0 +1,16 @@
+@extends('layout.master')
+
+
+@section('content')
+    <h1>Services</h1>
+@endsection
+
+
+@section('sidebar')
+<div class="card my-4">
+    <h5 class="card-header bg-danger">Services Side Widget</h5>
+    <div class="card-body">
+      You can put anything you want inside of these side widgets. They are easy to use, and feature the new Bootstrap 4 card containers!
+    </div>
+  </div>
+@endsection
