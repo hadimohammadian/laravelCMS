c8e822313f797d08d87e9009793f9ad6628619a6
Hadi mohammadian <hadimohammadian83@gmail.com>
2020-11-24 21:27:03 +0330

    Connect to db and get all data in articles table

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
+    $articles = DB::table('articles')->get();   
+    
+  
+    dd($articles);
     return view('welcome');
 });
 
